968d02a734bf8caa468ec881c8021501b56aa13c
Ayaphis <ayaphis@gmail.com>
2018-06-14 16:55:24 +0900

    Init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const koa = require('koa')
+const server = require('koa-static')
+const path = require('path')
+
+const app = new koa()
+
+const public = "./neet-code"
+
+app.use(server(
+    path.join(__dirname, public)
+))
+
+
+app.use(async (ctx) => {
+    ctx.body = "hello world"
+})
+
+app.listen(8080, () => {
+    console.log("statrt")
+})
